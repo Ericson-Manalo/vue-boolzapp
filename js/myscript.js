cc499bc345801d491db8adea06eba373b8149ade
Ericson-Manalo <ericmana98@gmail.com>
2023-06-08 23:51:13 +0200

    tryied to create a function toSendNewMessage

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,15 @@
-/*Milestone 1
-Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+/*
+Milestone 1
+    Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+    Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+    Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+    Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+    Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+    Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */ 
 
 
@@ -174,13 +183,27 @@ createApp({
         }
     ],
     activeIndex : 0,
+    newMessage : '',
 
         }
     },
     methods: {
         goToChat(chatIndex){
             this.activeIndex = chatIndex;
+        },
+
+        toSendNewMessage(elementIndex){
+            const newMessage={
+                date: '08/06/2023 20:20:00',
+                message: '',
+                status: 'sent'
+            }
+            if (newMessage != 0){
+                this.contacts[elementIndex].messages.push(newMessage);
+            }
+            this.newMessage='';
         }
     
     }
+
 }).mount('#app');
