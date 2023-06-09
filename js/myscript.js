89c6cfe3daaf024e3793253e881cce2d70030da9
Ericson-Manalo <ericmana98@gmail.com>
2023-06-09 11:58:36 +0200

    tryied to fix toSendNewMessage function

--- js/myscript.js
+++ js/myscript.js
@@ -10,6 +10,10 @@ Milestone 2
 Milestone 3
     Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
     Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+    Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
 */ 
 
 
@@ -121,7 +125,7 @@ createApp({
         },
         {
             name: 'Claudia',
-            avatar: './img/avatar_5.jpg',
+            avatar: './img/avatar_6.jpg',
             visible: true,
             messages: [
                 {
@@ -192,17 +196,26 @@ createApp({
             this.activeIndex = chatIndex;
         },
 
-        toSendNewMessage(elementIndex){
-            const newMessage={
+        toSendNewMessage(){
+            const userMessage={
                 date: '08/06/2023 20:20:00',
-                message: '',
+                message: this.newMessage,
                 status: 'sent'
             }
-            if (newMessage != 0){
-                this.contacts[elementIndex].messages.push(newMessage);
+            if (newMessage.message != 0){
+                this.contacts[0].messages.push(userMessage);
             }
-            this.newMessage='';
+            this.userMessage='';
+        },
+
+        contactAnswer(){
+            const contactAnsw={
+                date: '08/06/2023 20:21:00',
+                message: 'ok',
+                status: 'received'
+            };
         }
+
     
     }
 
